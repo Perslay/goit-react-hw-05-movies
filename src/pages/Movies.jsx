63b82b8bce812e58fb0a836fc47eac29ce2a396d
Natalia Fedyk <124808493+Perslay@users.noreply.github.com>
2024-05-12 01:18:41 +0200

    Search movies pages
    Buggy

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -3,10 +3,36 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export const Movies = ({ handleFetching }) => {
+  const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [submitted, setSubmitted] = useState(false);
+  const [pageNum, setPageNum] = useState(1);
 
   const navigate = useNavigate();
+
+  const showResults = async search => {
+    try {
+      const data = await handleFetching(
+        `https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=${pageNum}`
+      );
+      setSearchResults(data.results);
+      console.log(data.results);
+      setSubmitted(true);
+      navigate(`/movies`, { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBack = () => {
+    setPageNum(prevPageNum => Math.max(prevPageNum - 1, 1)); // Ensure pageNum doesn't go below 1
+    showResults(search);
+  };
+
+  const handleNext = () => {
+    setPageNum(prevPageNum => prevPageNum + 1);
+    showResults(search);
+  };
 
   const handleSubmit = async event => {
     event.preventDefault();
@@ -14,20 +40,11 @@ export const Movies = ({ handleFetching }) => {
     const form = event.currentTarget;
     const search = form.elements.search.value;
 
-    handleFetching(
-      `https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`
-    )
-      .then(data => {
-        setSearchResults(data.results);
-        console.log(data.results);
-      })
-      .then(() => {
-        setSubmitted(true);
-        navigate(`/movies`, { replace: true });
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    setSearch(search);
+
+    setPageNum(1);
+
+    await showResults(search);
 
     form.reset();
   };
@@ -40,13 +57,21 @@ export const Movies = ({ handleFetching }) => {
       </form>
       {submitted &&
         (searchResults.length > 0 ? (
-          <ul>
-            {searchResults.map(result => (
-              <li key={result.id}>
-                <Link to={`/movies/${result.id}`}>{result.title}</Link>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <ul>
+              {searchResults.map(result => (
+                <li key={result.id}>
+                  <Link to={`/movies/${result.id}`}>{result.title}</Link>
+                </li>
+              ))}
+            </ul>
+            <button onClick={handleBack} type="button">
+              Back
+            </button>
+            <button onClick={handleNext} type="button">
+              Next
+            </button>
+          </div>
         ) : (
           <p>There are not matching results.</p>
         ))}
